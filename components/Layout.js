fb98fc184d48fee8207d117895282b6179dd14d4
trizzi <patrickakhamiogu@gmail.com>
2022-06-30 16:12:31 +0100

    Header and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Header from './Header';
+4;
+import Footer from './Footer';
 import styles from '../styles/Layout.module.css';
 
 export default function Layout({
@@ -14,7 +17,10 @@ export default function Layout({
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
+      <Header />
+
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
