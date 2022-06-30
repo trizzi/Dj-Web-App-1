d24a28d151d4ad977ef5658cf019f62073c56813
trizzi <patrickakhamiogu@gmail.com>
2022-06-30 15:13:34 +0100

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <Layout title='About DJ Events'>
+      <h1>About</h1>
+      <p>
+        This is an app to find the latest DJs and other
+        musical events
+      </p>
+      <p>Version: 1.0.0</p>
+    </Layout>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function EventPage() {
+  return (
+    <Layout>
+      <h1>Slug page</h1>
+    </Layout>
+  );
+}
