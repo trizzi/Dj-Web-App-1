98fa67e47837a6875edf7f173ebe130c410b8a6d
trizzi <patrickakhamiogu@gmail.com>
2022-07-01 06:44:23 +0100

    Added initial client-side  API prototype

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  console.log(events);
+  return {
+    props: { events },
+  };
 }
